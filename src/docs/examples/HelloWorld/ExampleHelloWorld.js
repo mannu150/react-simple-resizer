8cfdf467396a32077f98826568e652c6abbf5c81
Mohit Anand <mohit@infoworks.io>
2019-03-23 14:04:15 +0530

    create custom docs app with HelloWorld example

---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import HelloWorld from 'react-simple-resizer/HelloWorld';
+
+/** Custom message */
+export default function ExampleHelloWorld() {
+	return <HelloWorld message="Avocode" />
+}
